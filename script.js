d04217cbaaff72d7e5c6555731d8ab3ffb8c4559
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-04-02 20:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+  let inputText = document.getElementById("text") 
+         let inputNumber = document.getElementById("delay") 
+         let btn = document.getElementById("btn")
+          let output = document.getElementById("output")
+         btn.addEventListener("click",(e)=>{
+            e.preventDefault()
+          let text = inputText.value
+          let delay = inputNumber.value
+
+
+
+
+
+
+          function genPromise(text,delay){
+            return new Promise((resolve,reject)=>{
+              setTimeout(()=>{
+                    resolve(text)
+              },delay)
+            })
+          }
+         
+         
+          async function delayingThings(text,delay){
+            let ans = await genPromise(text,delay)
+              let div = document.createElement("div")
+              div.innerText = ans;
+              output.append(div)
+          }
+          delayingThings(text,delay);
+        })
